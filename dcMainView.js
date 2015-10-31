d7c9be6c95dea2274b4f3c880c4af4f28822830f
Nathaniel Palmer <nathanielpalmer@me.com>
2015-10-31 01:39:29 -0400

    Implement adding devices with auto-generated names

--- dcMainView.js
+++ dcMainView.js
@@ -2,6 +2,22 @@ angular.module('dcApp', [])
 	.directive('dcMainView', function() {
 		return {
 			restrict: 'E',
-			templateUrl: 'dcMainView.html'
+			templateUrl: 'dcMainView.html',
+			controller: 'dcMainViewController',
+			controllerAs: 'vm',
+			scope: {}
 		};
+	})
+	.controller('dcMainViewController', function() {
+		var devices = [];
+		var counter = 0;
+
+		this.addButtonClicked = function() {
+			devices.push({name: 'device ' + counter});
+			counter++;
+		}
+
+		this.getDevices = function() {
+			return devices;
+		}
 	});
