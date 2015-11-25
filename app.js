4cb7a962ac466fd07db089a0bd19272ee9a599a8
Taylor, Zachary <ztaylor@follett.com>
2015-11-25 10:44:03 -0500

    Adding simple express server

---
+++ app.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+
+var server = app.listen(3000, function() {
+	var host = server.address().address;
+	var port = server.address().port;
+
+	console.log('App listening on http://' + host + ':' + port);
+});
+
+app.use(express.static('./'));
