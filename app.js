8e44129381a3ac3de2cd27a05d6300ececfa2c95
Taylor, Zachary <ztaylor@follett.com>
2015-11-25 11:01:47 -0500

    changing the connection status message for express

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var server = app.listen(3000, function() {
 	var host = server.address().address;
 	var port = server.address().port;
 
-	console.log('App listening on http://' + host + ':' + port);
+	console.log('App listening on http://localhost:' + port);
 });
 
 app.use(express.static('./'));
