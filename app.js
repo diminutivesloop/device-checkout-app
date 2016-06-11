5e153ea1878f9c97a034c61ffded761fd2e189b4
Nathaniel Palmer <nathanielpalmer@me.com>
2016-06-11 11:16:29 -0400

    Update app.js to defer all routing to client-side app

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var app = express();
 
+// app.use(express.static('deploy/'));
+app.get('*', function(req, res) {
+  res.sendfile('deploy/index.html');
+});
+
 var server = app.listen(3000, function() {
 	var host = server.address().address;
 	var port = server.address().port;
 
 	console.log('App listening on http://localhost:' + port);
 });
-
-app.use(express.static('deploy/'));
