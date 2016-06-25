1b06b8775451170439dfb3aaaf48d0516e960110
Zack Taylor <zacktayl@gmail.com>
2016-06-25 11:46:25 -0400

    Use glob library to provide globstar pattern matching for windows/osx

--- scripts/templateCachePrep.js
+++ scripts/templateCachePrep.js
@@ -1,9 +1,12 @@
 var templatecache = require('ng-templatecache');
 var fs = require('fs');
+var glob = require('glob');
 var templateArray = [];
 
-process.argv.slice(2).forEach(function(path, index) {
-	if(path.indexOf('index.html') === -1) {
+//Find all files under the specified directory
+glob.sync(process.argv[2] + '**').forEach(function(path, index) {
+  //Is the file extension html but not index.html
+	if(path.indexOf('.html') != -1 && path.indexOf('index.html') === -1) {
 		var contents = fs.readFileSync(path, 'utf8');
 		templateArray.push({content: contents, path: path})
 	}
@@ -14,7 +17,6 @@ var output = templatecache({
   module: 'dcApp',
   standalone: false
 });
-
 
 fs.writeFile("temp/templateCacheModule.js", output, function(err) {
     if(err) {
